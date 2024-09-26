8fb3b4a142555a605fb0b3dafb46cadd9cf73ad5
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-09-26 19:48:15 +0300

    fixed urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import movie_list, movie_detail
+from .views import movie_list_create, movie_detail_retrieve_update_delete
 
 app_name = "cinema"
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("movies/", movie_list_create, name="movie-list"),
+    path(
+        "movies/<int:pk>/",
+        movie_detail_retrieve_update_delete,
+        name="movie-detail"
+    ),
 ]
